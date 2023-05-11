d897fb89a86b491c0cbcf5776abac7f0c005ebe1
Rodrigo <contactorodrigocabrera@gmail.com>
2023-05-11 19:52:37 -0300

    se incorpora método para imprimir los valores en pantalla

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const displayValorAnterior = document.getElementsById('valor-anterior')
-const displayValorActual = document.getElementsById('valor-actual')
+const displayValorAnterior = document.getElementById('valor-anterior')
+const displayValorActual = document.getElementById('valor-actual')
 const botonesNumeros = document.querySelectorAll('.numero')
 const botonesOperadores = document.querySelectorAll('.operador')
+
+const display = new Display(displayValorAnterior, displayValorActual)
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => {
+        display.agregarNumero(boton.innerHTML)
+    })
+})
