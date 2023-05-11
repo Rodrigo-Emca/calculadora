8a474303bf6e29e2c23ecc7c9a1abc8f8e0396d1
Rodrigo <contactorodrigocabrera@gmail.com>
2023-05-11 19:35:02 -0300

    se crea javaScript para funcionalidades

---
+++ index.js
@@ -0,0 +1,4 @@
+const displayValorAnterior = document.getElementsById('valor-anterior')
+const displayValorActual = document.getElementsById('valor-actual')
+const botonesNumeros = document.querySelectorAll('.numero')
+const botonesOperadores = document.querySelectorAll('.operador')
