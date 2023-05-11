541c391953d0e610ae9e7c18e93c0e3aa2e2a32a
Rodrigo <contactorodrigocabrera@gmail.com>
2023-05-11 20:08:02 -0300

    se setean los operadores matematicos

--- Display.js
+++ Display.js
@@ -30,4 +30,12 @@ class Display {
         this.displayValorActual.textContent = this.valorActual;
         this.displayValorAnterior.textContent = this.valorAnterior;
     }
+
+    calcular() {
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const valorActual = parseFloat(this.valorActual);
+
+        if( isNaN(valorActual) || (valorAnterior)) return
+        this.valorActual = this.calculador[this.tipoOperacion] (valorAnterior, valorActual);
+    }
 }
--- index.js
+++ index.js
@@ -10,3 +10,7 @@ botonesNumeros.forEach(boton => {
         display.agregarNumero(boton.innerHTML)
     })
 })
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value)) 
+})
